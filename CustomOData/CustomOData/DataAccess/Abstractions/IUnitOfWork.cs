78b042a611eea584bbd674f0a00a68cd7ad47724
Muhammad Razali <wireless-@hotmail.com>
2019-12-06 23:36:06 +0800

    Created IUnitofWork and UnitOfWork

---
+++ CustomOData/CustomOData/DataAccess/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomOData.DataAccess.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        void Begin();
+        void Close();
+        void Commit();
+        void Dispose();
+        void Rollback();
+    }
+}
---
+++ CustomOData/CustomOData/DataAccess/UnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using CustomOData.DataAccess.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CustomOData.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public void Begin()
+        {
+            if (_dbConnection.State == ConnectionState.Closed)
+            {
+                _dbConnection.Open();
+            }
+            _dbTransaction = _dbConnection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            _dbTransaction.Commit();
+            Dispose();
+        }
+
+        public void Rollback()
+        {
+            _dbTransaction.Rollback();
+            Dispose();
+        }
+
+        public void Close()
+        {
+            if (_dbConnection.State != ConnectionState.Closed)
+            {
+                _dbConnection.Close();
+            }
+        }
+        public void Dispose()
+        {
+            Close();
+
+            if (_dbTransaction != null)
+            {
+                _dbTransaction.Dispose();
+            }
+
+            if (_dbConnection != null)
+            {
+                _dbConnection.Dispose();
+            }
+
+
+            _dbTransaction = null;
+            _dbConnection = null;
+        }
+
+        private IDbConnection _dbConnection;
+        private IDbTransaction _dbTransaction;
+    }
+}

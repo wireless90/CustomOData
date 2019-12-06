a1fcb1eee0b85231cf8136b465b715fa57b89a28
Muhammad Razali <wireless-@hotmail.com>
2019-12-06 22:49:49 +0800

    Employee controller now gets dummy data

---
+++ CustomOData/CustomOData/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using CustomOData.Models;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomOData.Controllers
+{
+    public class EmployeesController : ODataController
+    {
+        // GET: Employees
+        [HttpGet]
+        [EnableQuery]
+        public List<Employee> Get(ODataQueryOptions oDataQueryOptions)
+        {
+            return new List<Employee>() { new Employee() { Age = 10, Id = 1, Name = "Razali" } };
+        }
+    }
+}

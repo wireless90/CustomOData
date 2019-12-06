b4087ecba05fe0024b7206d64e13439f7758bcdb
Muhammad Razali <wireless-@hotmail.com>
2019-12-06 22:35:16 +0800

    Created Employee and Salary classes

---
+++ CustomOData/CustomOData/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomOData.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public Salary Salary { get; set; }
+    }
+}
---
+++ CustomOData/CustomOData/Models/Salary.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomOData.Models
+{
+    public class Salary
+    {
+        public int Id { get; set; }
+        public int Wage { get; set; }
+    }
+}

9d709208b7564685c5059aaaa55c35fe4f7c755e
Muhammad Razali <wireless-@hotmail.com>
2019-12-06 22:58:30 +0800

    Created WebApiModule for Autofac

---
+++ CustomOData/CustomOData/App_Start/IOC/ContainerConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomOData.App_Start.IOC
+{
+    public static class ContainerConfig
+    {
+    }
+}

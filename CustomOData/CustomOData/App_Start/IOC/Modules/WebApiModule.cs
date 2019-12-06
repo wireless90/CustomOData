40fc83a7986c9412bc5ca5aa73acb7140b38a8f6
Muhammad Razali <wireless-@hotmail.com>
2019-12-06 22:58:48 +0800

    Created WebApiModule for autofac

---
+++ CustomOData/CustomOData/App_Start/IOC/Modules/WebApiModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using Module = Autofac.Module;
+
+namespace CustomOData.App_Start.IOC.Modules
+{
+    public class WebApiModule:Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            //Example from
+            //https://autofaccn.readthedocs.io/en/latest/integration/webapi.html#quick-start
+            // Get your HttpConfiguration.
+            HttpConfiguration httpConfiguration = GlobalConfiguration.Configuration;
+
+            // Register your Web API controllers.
+            containerBuilder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            containerBuilder.RegisterWebApiFilterProvider(httpConfiguration);
+
+            // OPTIONAL: Register the Autofac model binder provider.
+            containerBuilder.RegisterWebApiModelBinderProvider();
+
+            base.Load(containerBuilder);
+        }
+    }
+}

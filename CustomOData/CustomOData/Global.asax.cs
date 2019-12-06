5a6cd09e4d927943f7329332e0c3c3dec3cc061d
Muhammad Razali <wireless-@hotmail.com>
2019-12-06 23:05:24 +0800

    Added Autofac to Global.Asax

--- CustomOData/CustomOData/Global.asax.cs
+++ CustomOData/CustomOData/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using CustomOData.App_Start.IOC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +16,10 @@ namespace CustomOData
     {
         protected void Application_Start()
         {
+            IContainer container = ContainerConfig.BuildContainer();
+            HttpConfiguration httpConfiguration = GlobalConfiguration.Configuration;
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
